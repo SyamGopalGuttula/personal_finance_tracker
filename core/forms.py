dd389de294cbf28de7b2baacf9cd2a01c01c9d42
Syam Gopal Guttula <syamgopal.guttula@gmail.com>
2025-05-12 15:17:45 -0400

    Added custom CSS styling and static file handling, improved form validation

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import Expense
+from datetime import date
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['title', 'amount', 'date', 'category', 'description']
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount <= 0:
+            raise forms.ValidationError("Amount must be greater than zero.")
+        return amount
+
+    def clean_date(self):
+        expense_date = self.cleaned_data.get('date')
+        if expense_date > date.today():
+            raise forms.ValidationError("Date cannot be in the future.")
+        return expense_date
--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6b*g$n45qks2q6r^px*w4$moex5n2xayd$u^rjn$l6wqb-m0a%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -76,7 +80,7 @@ WSGI_APPLICATION = 'finance_tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / os.getenv('DATABASE_NAME'),
     }
 }
 
@@ -116,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'core/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # This is where all static files will be collected
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
